fc7b51832b03822f4ad61bafd603100c8cef02b4
David Kettler <21echoes@gmail.com>
2023-04-24 12:42:21 -0700

    Put output in a textarea, click button to copy it or save it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
 import { BANK_NUM_PATTERNS, Bank } from '@/core/bank';
 import { convertMidiFilesToBank } from '@/core/midi-file-reader';
 import Head from 'next/head'
-import { FormEvent, useCallback, useState } from 'react'
+import { FormEvent, useCallback, useRef, useState } from 'react'
 import arrayBufferToBuffer from 'arraybuffer-to-buffer'
 import NoteMapper from '@/components/NoteMapper';
 import FileSelector from '@/components/FileSelector';
 import { DefaultMidiNoteMap, MidiNoteMap } from '@/core/midi-note-map';
 import { Track } from '@/core/track';
+import FileSaver from 'file-saver';
 
-const getBuffersFromFormFiles = async (form: HTMLFormElement): Promise<(Buffer|undefined)[]> =>
+const getBuffersFromFormFiles = async (form: HTMLFormElement): Promise<(Buffer | undefined)[]> =>
   Promise.all((new Array(BANK_NUM_PATTERNS).fill(0)).map((_, i) =>
-    new Promise<Buffer|undefined>((resolve) => {
+    new Promise<Buffer | undefined>((resolve) => {
       const input = form[i] as HTMLInputElement;
 
       const file = input.files?.[0];
@@ -54,6 +55,27 @@ export default function Home() {
     setBankString(bank.toString());
   }, [midiNoteMapEntries]);
 
+  const bankTextAreaRef = useRef<HTMLTextAreaElement>(null);
+  const copyText = async (text: string): Promise<void> => navigator.clipboard.writeText(text);
+  const copyBank: React.MouseEventHandler = useCallback((e) => {
+    e.preventDefault();
+    if (!bankTextAreaRef.current) {
+      return;
+    }
+    copyText(bankTextAreaRef.current.value).catch(console.error);
+  }, []);
+  const saveText = (text: string) => {
+    const blob = new Blob([text], {type: "text/csv;charset=us-ascii"});
+    FileSaver.saveAs(blob, "PatternBank000.txt");
+  }
+  const saveBank: React.MouseEventHandler = useCallback((e) => {
+    e.preventDefault();
+    if (!bankTextAreaRef.current) {
+      return;
+    }
+    saveText(bankTextAreaRef.current.value);
+  }, []);
+
   return (
     <>
       <Head>
@@ -64,15 +86,24 @@ export default function Home() {
           MIDI files:
           <FileSelector />
 
-          <br/>
+          <br />
           MIDI note mapping:
           <NoteMapper midiNoteMapEntries={midiNoteMapEntries} setMidiNoteMapEntries={setMidiNoteMapEntries} />
 
-          <br/>
+          <br />
           <button type="submit">Generate</button>
         </form>
 
-        {bankString ? <pre>{bankString}</pre> : null}
+        {bankString ? (
+          <div>
+            <br />
+            <button onClick={copyBank}>Copy</button>
+            <button onClick={saveBank}>Save</button>
+            <div>
+              <textarea rows={27} cols={90} ref={bankTextAreaRef} value={bankString} />
+            </div>
+          </div>
+        ) : null}
       </main>
     </>
   )
